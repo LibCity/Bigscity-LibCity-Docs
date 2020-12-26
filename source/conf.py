49618bd33b179f783796a117ed7da06416809d7f
jjw <jwjiang_buaa@163.com>
2020-12-26 23:23:40 +0800

    init

--- source/conf.py
+++ source/conf.py
@@ -29,8 +29,8 @@ author = 'aptx1231'
 # ones.
 extensions = [
 'sphinx_rtd_theme',
-'sphinx_markdown_tables',
 'recommonmark',
+'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
