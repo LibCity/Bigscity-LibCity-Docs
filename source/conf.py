fba58cf01efac40b33c283dda50f046dbea80d57
jjw <jwjiang_buaa@163.com>
2021-06-27 23:25:50 +0800

    pdf

--- source/conf.py
+++ source/conf.py
@@ -68,3 +68,23 @@ source_parsers = {
 source_suffix = ['.rst', '.md']
 
 napoleon_include_private_with_doc = True
+
+latex_elements={
+    # The paper size ('letterpaper' or 'a4paper').
+    'papersize':'a4paper',# The font size ('10pt', '11pt' or '12pt').
+    'pointsize':'12pt','classoptions':',oneside','babel':'',#必須
+    'inputenc':'',#必須
+    'utf8extra':'',#必須
+    # Additional stuff for the LaTeX preamble.
+    'preamble': r"""
+    \usepackage{xeCJK}
+    \usepackage{indentfirst}
+    \setlength{\parindent}{2em}
+    \setCJKmainfont{WenQuanYi Micro Hei}
+    \setCJKmonofont[Scale=0.9]{WenQuanYi Micro Hei Mono}
+    \setCJKfamilyfont{song}{WenQuanYi Micro Hei}
+    \setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
+    \XeTeXlinebreaklocale "zh"
+    \XeTeXlinebreakskip = 0pt plus 1pt
+    """
+}
