31608e606a29d5182fe2aaa874c0bff27826004f
jjw <jwjiang_buaa@163.com>
2021-09-20 16:11:55 +0800

    fix bug

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
-autodoc_mock_imports = ["torch"]
 import sphinx_rtd_theme
 from recommonmark.parser import CommonMarkParser
 # -- Project information -----------------------------------------------------
@@ -42,6 +41,7 @@ extensions = [
 'sphinx.ext.intersphinx',
 'sphinx.ext.viewcode',
 ]
+autodoc_mock_imports = ["torch", "torch_scatter"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
